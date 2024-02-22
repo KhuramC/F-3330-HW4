64a2c5f87923cf35999bc1abad0614c63d4b8206
Alon-Barzilay <alonbarzilay0@gmail.com>
2024-02-21 19:38:12 -0600

    Added getters/setters and copy constructor to Vehicle class.

--- Homework_4/src/vehicles/Vehicle.java
+++ Homework_4/src/vehicles/Vehicle.java
@@ -13,4 +13,102 @@ public abstract class Vehicle {
     protected double gasTankCapacity;
     protected StartMechanism startType;
 
+
+    public Vehicle(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+    	super();
+		this.brand = brand;
+		this.make = make;
+		this.modelYear = modelYear;
+		this.price = price;
+		this.color = color;
+		this.fuelType = fuelType;
+		this.mileage = mileage;
+		this.mass = mass;
+		this.cylinders = cylinders;
+		this.gasTankCapacity = gasTankCapacity;
+		this.startType = startType;
+	}
+    
+    //copy constructor
+    public Vehicle(Vehicle v) {
+    	this.brand = v.getBrand();
+		this.make = v.getMake();
+		this.modelYear = v.getModelYear();
+		this.price = v.getPrice();
+		this.color = v.getColor();
+		this.fuelType = v.getFuelType();
+		this.mileage = v.getMileage();
+		this.mass = v.getMass();
+		this.cylinders = v.getCylinders();
+		this.gasTankCapacity = v.getGasTankCapacity();
+		this.startType = v.getStartType();
+    }
+    
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public long getModelYear() {
+		return modelYear;
+	}
+	public void setModelYear(long modelYear) {
+		this.modelYear = modelYear;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public VehicleColor getColor() {
+		return color;
+	}
+	public void setColor(VehicleColor color) {
+		this.color = color;
+	}
+	public FuelType getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(FuelType fuelType) {
+		this.fuelType = fuelType;
+	}
+	public double getMileage() {
+		return mileage;
+	}
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
+	public double getMass() {
+		return mass;
+	}
+	public void setMass(double mass) {
+		this.mass = mass;
+	}	
+	public int getCylinders() {
+		return cylinders;
+	}
+	public void setCylinders(int cylinders) {
+		this.cylinders = cylinders;
+	}
+	public double getGasTankCapacity() {
+		return gasTankCapacity;
+	}
+	public void setGasTankCapacity(double gasTankCapacity) {
+		this.gasTankCapacity = gasTankCapacity;
+	}
+	public StartMechanism getStartType() {
+		return startType;
+	}
+	public void setStartType(StartMechanism startType) {
+		this.startType = startType;
+	}
 }
