5992fa22ee7e3bf0de38e50d7cce54403e9324db
Alon-Barzilay <alonbarzilay0@gmail.com>
2024-02-21 21:48:58 -0600

    Added abstract classes calculateMaintenanceCost, calculateFuelEfficiency, startEngine, and toString to Car.

--- Homework_4/src/vehicles/Car.java
+++ Homework_4/src/vehicles/Car.java
@@ -6,6 +6,22 @@ public class Car extends Vehicle {
 			 double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
 		super( brand,  make,  modelYear,  price, color,  fuelType,
 				  mileage,  mass,  cylinders,  gasTankCapacity,  startType); 
-
 	}
+	public double calculateMaintenanceCost(double distance) {
+		double cost = distance * mass * (Vehicle.currentYear-modelYear) * cylinders * 0.0005; // given equation
+		return cost;
+	}
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		double fuelEfficiency = cylinders * gasTankCapacity * fuelPrice / distance * 0.003; // given equation
+		return fuelEfficiency;
+	}
+	public void startEngine() {
+		System.out.println("The car has been started by " + startType);
+	}
+	@Override
+	public String toString() {
+		return "The type of this vehicle is Car and has brand=" + brand + ", make=" + make + ", modelYear=" + modelYear + ", price=" + price + ", color="
+				+ color + ", fuelType=" + fuelType + ", mileage=" + mileage + ", mass=" + mass + ", cylinders="
+				+ cylinders + ", gasTankCapacity=" + gasTankCapacity + ", and startType=" + startType;
+	}	
 }
