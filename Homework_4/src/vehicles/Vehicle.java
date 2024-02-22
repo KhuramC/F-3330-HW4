e50c5b18dcf1e07c9751a2d30c4eebcea31e3365
hipolitoIV <80554014+hipolitoIV@users.noreply.github.com>
2024-02-21 18:54:07 -0600

    Vehicle abstract methods added to Vehicle class

--- Homework_4/src/vehicles/Vehicle.java
+++ Homework_4/src/vehicles/Vehicle.java
@@ -12,5 +12,9 @@ public abstract class Vehicle {
     protected int cylinders;
     protected double gasTankCapacity;
     protected StartMechanism startType;
-
+    
+    public abstract double calculateMaintenaceCost(double distance); // calculates maintenance cost for a specific vehicle
+    public abstract double calculateFuelEfficiency(double distance, double fuelPrice); //calculates the engine efficiency
+    public abstract void startEngine(); // prints how the vehicle starts
+    
 }
