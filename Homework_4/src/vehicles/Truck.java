fdf05e7cd047cafc7255b18e78fb9b2c93c7968a
Alon-Barzilay <alonbarzilay0@gmail.com>
2024-02-22 16:14:35 -0600

    Added abstract methods to truck class

--- Homework_4/src/vehicles/Truck.java
+++ Homework_4/src/vehicles/Truck.java
@@ -7,5 +7,22 @@ public class Truck extends Vehicle {
 		super( brand,  make,  modelYear,  price, color,  fuelType,
 				  mileage,  mass,  cylinders,  gasTankCapacity,  startType);
 	}
+	public double calculateMaintenanceCost(double distance) {
+		double cost = distance * mass * (Vehicle.currentYear-modelYear) * cylinders * 0.002; // given equation
+		return cost;
+	}
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		double fuelEfficiency = cylinders * gasTankCapacity * fuelPrice / distance * 0.1; // given equation
+		return fuelEfficiency;
+	}
+	public void startEngine() {
+		System.out.println("The car has been started by " + startType);
+	}
+	@Override
+	public String toString() {
+		return "The type of this vehicle is Truck and has brand=" + brand + ", make=" + make + ", modelYear=" + modelYear + ", price=" + price + ", color="
+				+ color + ", fuelType=" + fuelType + ", mileage=" + mileage + ", mass=" + mass + ", cylinders="
+				+ cylinders + ", gasTankCapacity=" + gasTankCapacity + ", and startType=" + startType;
+	}
 
 }
