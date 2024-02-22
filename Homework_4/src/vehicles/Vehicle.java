f65c29ebb676b20405e926486ab73d1e6bd9b9a7
hipolitoIV <80554014+hipolitoIV@users.noreply.github.com>
2024-02-21 19:09:10 -0600

    Fixed typo in calculateMaintenanceCost

--- Homework_4/src/vehicles/Vehicle.java
+++ Homework_4/src/vehicles/Vehicle.java
@@ -13,7 +13,7 @@ public abstract class Vehicle {
     protected double gasTankCapacity;
     protected StartMechanism startType;
     
-    public abstract double calculateMaintenaceCost(double distance); // calculates maintenance cost for a specific vehicle
+    public abstract double calculateMaintenanceCost(double distance); // calculates maintenance cost for a specific vehicle
     public abstract double calculateFuelEfficiency(double distance, double fuelPrice); //calculates the engine efficiency
     public abstract void startEngine(); // prints how the vehicle starts
     
